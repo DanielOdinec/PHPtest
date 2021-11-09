41ebc5a762ccfb33b493f15c004b202a1e928ad5
opilane <odinec.danik@gmail.com>
2021-11-09 10:17:51 +0200

    Ülesanne 403 on tehtud

---
+++ harjutused/403.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Ülesanne 403</title>
+</head>
+<body>
+<h1>Ülesanne 403</h1>
+<?php
+$varv=array('Gold','Azure','Cyan','Coral','Indigo','Ivory','Khaki','Lime','Linen','Maroon','Magenta','Navy','Olive','Orchid','Plum');
+for($i =0; $i <15; $i++){
+    echo "<span style='color: $varv[$i]' >$varv[$i]</span><br>";
+}
+
+
+?>
+</body>
+</html>
+
